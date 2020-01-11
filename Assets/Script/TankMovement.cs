f54866a6494839dce96624e6f2002cb71406883f
Robin Cho <robin.cho@mail.mcgill.ca>
2020-01-11 00:17:19 -0500

    Tank Movement v.0.5
    Movement and Turning for Tanks

--- Assets/Script/TankMovement.cs
+++ Assets/Script/TankMovement.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int playerNumber = 1;
+    public float rotateSpeed = 180f;
+    public float speed = 12f;
+
+    private string movementAxis;
+    private string turnAxis;
+    private Rigidbody tankRigidBody;
+    private float movementInputValue;
+    private float turnInputValue;
+
+    private void Awake()
     {
-        
+        tankRigidBody = this.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 2.5f));
+        tankRigidBody.isKinematic = false;
+        movementInputValue = 0f;
+        turnInputValue = 0f;
+    }
+
+    private void OnDisable()
+    {
+        tankRigidBody.isKinematic = true;
+    }
+
+    private void Start()
+    {
+        movementAxis = "Vertical" + playerNumber;
+        turnAxis = "Horizontal" + playerNumber;
+    }
+
+    private void Update()
+    {
+        movementInputValue = Input.GetAxis(movementAxis);
+        turnInputValue = Input.GetAxis(turnAxis);
+
+    }
+
+    private void FixedUpdate()
+    {
+        TankMove();
+        TankTurn();
+    }
+
+    private void TankMove()
+    {
+        Vector3 movement = transform.forward * movementInputValue * speed * Time.deltaTime;
+        tankRigidBody.MovePosition(tankRigidBody.position + movement);
+    }
+
+    private void TankTurn()
+    {
+        float turn = turnInputValue * rotateSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        tankRigidBody.MoveRotation(tankRigidBody.rotation * turnRotation);
     }
 }
+
