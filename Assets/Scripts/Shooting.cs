b9412b79168631a17c654f967672b9ffcab36065
Robin Cho <robin.cho@mail.mcgill.ca>
2020-01-11 13:09:42 -0500

    Tank Turret Correction
    Fixed point turret
    You may modify to move the turret seperately

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [Range(1, 2)]
+    public int playerNumber = 1;
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private string firePlayer;
+
+    [Range(5f, 30f)]
+    public float bulletForce = 20f;
+
+    void Start()
+    {
+        firePlayer = "Fire" + playerNumber;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown(firePlayer))
+        {
+            Shoot();
+        }
+    }
+
+    // Shooting action
+    void Shoot()
+    {
+        // Create a bullet
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        // Bullet flies at a high velocity
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
+
+
---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TankMovement : MonoBehaviour
+{
+    [Range(1, 2)]
+    public int playerNumber = 1;
+
+    [Range(2f, 4f)]
+    public float rotateSpeed = 2.5f;
+    [Range(1f, 4f)]
+    public float speed = 2f;
+    [Range(2f, 4f)]
+    public float cannonRotateSpeed = 3.0f;
+
+    private string movementAxis;
+    private string turnAxis;
+    private string cannonAxis;
+    private Rigidbody2D tankRigidBody;
+    private float movementInputValue;
+    private float turnInputValue;
+    private float cannonTurnValue;
+    private Transform tankCannon;
+
+    private void Awake() {
+        tankRigidBody = this.GetComponent<Rigidbody2D>();
+        tankCannon = transform.Find("Cannon");
+    }
+
+    private void OnEnable() {
+        tankRigidBody.isKinematic = false;
+        movementInputValue = 0f;
+        turnInputValue = 0f;
+    }
+
+    private void OnDisable()
+    {
+        tankRigidBody.isKinematic = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementAxis = "Vertical" + playerNumber;
+        turnAxis = "Horizontal" + playerNumber;
+        cannonAxis = "HorizontalCannon" + playerNumber;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInputValue = Input.GetAxisRaw(movementAxis);    // vertical
+        turnInputValue = Input.GetAxisRaw(turnAxis);       // horizontal
+        cannonTurnValue = Input.GetAxisRaw(cannonAxis);
+    }
+
+    private void FixedUpdate() {
+        TankMove();
+        TankTurn();
+       //CannonTurn();
+    }
+
+    private void TankMove() {
+        tankRigidBody.velocity = transform.up * speed * Mathf.Clamp01(movementInputValue);
+    }
+
+    private void TankTurn()
+    {
+        float turn = -turnInputValue * rotateSpeed;
+        this.transform.Rotate(Vector3.forward * turn);
+    }
+
+    private void CannonTurn()
+    {
+        float turn = -turnInputValue * rotateSpeed;
+        tankCannon.Rotate(Vector3.forward * turn, Space.World);
+    }
+}
